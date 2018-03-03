5750742393307274e50e054651a8071180fd706f
Teo-Orthlieb <teo.orthlieb1@uqac.ca>
2018-03-03 01:39:24 -0500

    Materiaux de base + cible

---
+++ Assets/Scripts/CibleBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CibleBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.tag == "Balle") {
+			Debug.Log ("la balle touche la cible");
+		} else {
+			Debug.Log ("autre chose touche la cible ?!? (" + collider.name + ")" );
+		}
+	}
+}
