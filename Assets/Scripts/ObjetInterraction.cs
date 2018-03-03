81f00c1f8fe92ed2f2e357a5cf24335c01dcf46b
Lucas <kitolex.soluce@gmail.com>
2018-03-03 18:48:32 -0500

    objetInteraction

---
+++ Assets/Scripts/ObjetInterraction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjetInterraction : MonoBehaviour {
+
+	public GameManager gameManager;
+	public bool canUpdate;
+
+	void Awake(){
+
+	}
+
+	// Use this for initialization
+	public void Start () {
+		canUpdate = true;
+		gameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+		gameManager.myaction += setUpdate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setUpdate(bool etat){
+		canUpdate = etat;
+		gameManager.myaction -= setUpdate;
+	}
+
+	public void ternirObjet(){
+
+	}
+}
