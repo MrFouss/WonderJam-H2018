d4b8e2f2f8401abae528d3736dae7d5b8be75bd1
Ezia <esia.belbachir@protonmail.com>
2018-03-03 14:22:40 -0500

    Allow editor collision

--- Assets/Scripts/Hud/HudManager.cs
+++ Assets/Scripts/Hud/HudManager.cs
@@ -130,7 +130,7 @@ public class HudManager : MonoBehaviour {
             {
                 // manipulate na object
                 CollisionChecker check = spawnedObject.GetComponent<CollisionChecker>();
-                if (check.collisions == 0 && check.triggers == 0)
+                if (/*check.collisions == 0 && check.triggers == 0*/true)
                 {
                     spawnedObjectRenderer.material = spawnedObjectRealMaterial;
                     
