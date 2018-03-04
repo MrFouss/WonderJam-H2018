6d659da70a1738a38b3124ba2e4f6fbb4dbeb9ab
Fiesta87 <jb@pommeret.fr>
2018-03-04 09:40:58 -0500

    la ciblefait des beats visuel et sonor

--- Assets/Scripts/CibleBehaviour.cs
+++ Assets/Scripts/CibleBehaviour.cs
@@ -4,14 +4,48 @@ using UnityEngine;
 
 public class CibleBehaviour : MonoBehaviour {
     Random random;
+	[Tooltip("en battements par seconde")]
+	public float beatRate;
+	public float dureeBeat;
+	public float amplitudeBeat;
+	public AudioClip son;
 
 	private GameManager gameManager;
+	private Vector3 originalScale;
+	private float timerStartNextBeat;
+	// private float timerEndNextBeat;
+	private bool inBeat;
+	private float timeStartActualBeat;
+	private AudioSource source;
 
 	void Start () {
+		source = GetComponent<AudioSource>();
 		gameManager = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+		originalScale = this.transform.localScale;
+		inBeat = false;
+		this.startBeat();
 	}
 
 	void Update () {
+		if(Time.time >= timerStartNextBeat && !inBeat){
+			startBeat();
+		}
+
+		if(inBeat){
+
+			float sin = Mathf.Sin((Time.time - timeStartActualBeat) * Mathf.PI / dureeBeat);
+
+			if(sin >= 0.0f){
+				this.transform.localScale = originalScale * (1.0f + sin * amplitudeBeat);
+			}
+			else {
+				this.transform.localScale = originalScale;
+				inBeat = false;
+			}
+		}
+	}
+
+	void FixedUpdate() {
 		
 	}
 
@@ -24,5 +58,19 @@ public class CibleBehaviour : MonoBehaviour {
                 gameManager.changeCible();
             }
 		}
+	}
+
+	private void startBeat(){
+		
+		timeStartActualBeat = Time.time;
+		inBeat = true;
+		source.PlayOneShot(son, 0.5f);
+
+		this.planifNextBeat();
+	}
+
+	private void planifNextBeat(){
+		timerStartNextBeat = Time.time + 1.0f / beatRate - dureeBeat / 2.0f;
+		// timerEndNextBeat = timerStartNextBeat + dureeBeat;
 	}
 }
